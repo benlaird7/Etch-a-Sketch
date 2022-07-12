6a5229fe1391d5097ab1d5b2f777849cc9acf327
benlaird7 <benjaminoliverlaird@gmail.com>
2022-07-12 12:11:28 +0100

    centered etch-a-sketch

--- script.js
+++ script.js
@@ -1,25 +1,14 @@
-// In this document I am going to be creating an etch a sketch! To do this I will need to create a 16 x 16 sqaure of divs
-// Which can change colors when you hover over them
-
-
 // The container for the Divs
 const container = document.getElementById("container");
 
-// Divs
-    let verticalDiv = document.createElement('div');
-    verticalDiv.className = 'vertical';
+// Function to create square block
 
-    let horizontalDiv = document.createElement('div')
-    horizontalDiv.className = 'horizontal';
-
-// Functions to create square block
-
-  sqaureBlock = (rows, cols) => {
+sqaureBlock = (rows, cols) => {
   container.style.setProperty('--grid-rows', rows);
   container.style.setProperty('--grid-cols', cols);
   for (c = 0; c < (rows * cols); c++) {
     let cell = document.createElement("div");
-    container.appendChild(cell).className = "grid-item";
+    container.appendChild(cell).id = "grid-item";
   };
 };
 
